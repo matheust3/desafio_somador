0d37865e300299a659db9961dade68ca4c7332d0
Matheus Toniolli <matheus.toniolli@hotmail.com>
2022-01-25 12:59:20 -0400

    chore: challenge implementation

---
+++ somador.js
@@ -0,0 +1,19 @@
+var soma = (num) => {
+  var sum = 0;
+  if (!Number.isInteger(num)) {
+    throw Error('O número passado não é um número inteiro');
+  } else if (num < 0) {
+    throw Error('O número passado não é um número positivo');
+  } else {
+    for (let i = num - 1; i > 0; i--) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
+
+var entrada = 11; // Substitua 11 por outro valor
+
+console.log(soma(entrada));
